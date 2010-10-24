43d7df81673ea8c3ac6dbfd3c35f0177933df3e7
Justin Buchanan <justbuchanan@gmail.com>
2010-10-23 20:54:53 -0400

    worked on controller files

--- Common/Controller.c
+++ Common/Controller.c
@@ -1,7 +1,7 @@
 
 
 #ifndef _JoystickDriver_
-#include "joystickDriver.c"
+#include "JoystickDriver.c"
 #endif
 
 
@@ -13,12 +13,7 @@
 
 
 
-
-
-
-
-#define kJoystickConversionFactor (100.0/128.0)
-
+#define kJoystickConversionFactor (1.0/128.0)
 
 
 
@@ -26,24 +21,63 @@ void _UpdateControllerButtons(Controller& c, short buttons)
 {
 	c.buttonsPrevious = c.buttons;
 	c.buttons = buttons;
-
+	
 	short changedButtons = c.buttonsPrevious ^ c.buttons;	//	XOR operation gives us all the buttons that changed
-
-	if ( changedButtons > 0 )
-	{
-	    nxtDisplayCenteredTextLine(2, "true");
-	    wait10Msec(50);
-	 }
+	c.toggleButtons = c.toggleButtons ^ (changedButtons & buttons);	//	toggle the changed buttons that became 'true'
+}
 
 
-	short temp = !c.toggleButtons;
-	temp = temp ^ (changedButtons & buttons);	//	toggle the changed buttons that became 'true'
-	c.toggleButtons = !temp;
+void _UpdateControllerDPad(Controller& c, short dPadValue)
+{
+	/*
+	
+	DPad values from joystick
+	
+	-1 = (0,0) = not pressed
+	0  = (0,1)
+	1  = (1,1)
+	2  = (1,0)
+	3  = (1,-1)
+	4  = (0,-1)
+	5  = (-1,-1)
+	6  = (-1,0)
+	7  = (-1,1)
+	
+	*/
+	
+	
+	
+	/*	the x & y values are in arrays so they can be looked up quickly	*/
+	static float dPadXValues[] = {
+		0, 0, 1, 1, 1, 0, -1, -1, -1
+	};
+	
+	static float dPadYValues[] = {
+		0, 1, 1, 0, -1, -1, -1, 0, 1
+	};
+	
+	
+	Vector dPad;
+	Vector2DMake(dPadXValues[dPadValue + 1], dPadYValues[dPadValue + 1], dPad);
+	memcpy(c.dPad, dPad, sizeof(Vector));
 }
 
 
 
+bool ControllerButtonIsPressed(Controller& c, ControllerButton b)
+{
+	return c.buttons & (1 << b);
+}
 
+bool ControllerButtonChanged(Controller& c, ControllerButton b)
+{
+	return (c.buttons ^ c.previousButtons) & (1 << b);
+}
+
+bool ControllerButtonIsToggledOn(Controller& c, ControllerButton b)
+{
+	return c.toggledButtons & (1 << b);
+}
 
 
 
@@ -51,16 +85,16 @@ void UpdatePrimaryController(Controller& c)
 {
 	//	get the joystick info
 	Vector left, right;
-	Vector2DMake(joystick.joy1_x1 * kJoystickConversionFactor, joystick.joy1_y1 * kJoystickConversionFactor, left);
-	Vector2DMake(joystick.joy1_x2 * kJoystickConversionFactor, joystick.joy1_y2 *kJoystickConversionFactor, right);
+	Vector2DMake(joystick.joy1_x1 * kJoystickConversionFactor, joystick.joy1_y1 * kJoystickConversionFactor * -1.0, left);
+	Vector2DMake(joystick.joy1_x2 * kJoystickConversionFactor, joystick.joy1_y2 * kJoystickConversionFactor * -1.0, right);
 
 	memcpy(c.leftJoystick, left, sizeof(Vector));
 	memcpy(c.rightJoystick, right, sizeof(Vector));
 
-	if ( right.x > 0 ) PlaySound(soundBeepBeep);
-
-	//	FIXME: D-Pad?????????????????????????????????????????
-
+	//	get D-Pad
+	_UpdateControllerDPad(c, joystick.joy1_TopHat);
+	
+	//	get buttons
 	_UpdateControllerButtons(c, joystick.joy1_Buttons);
 }
 
@@ -68,15 +102,19 @@ void UpdatePrimaryController(Controller& c)
 void UpdateSecondaryController(Controller& c)
 {
 	//	get the joystick info
-  Vector left, right;
-	Vector2DMake(joystick.joy2_x1 * kJoystickConversionFactor, joystick.joy2_y1 * kJoystickConversionFactor, left);
-	Vector2DMake(joystick.joy2_x2 * kJoystickConversionFactor, joystick.joy2_y2 *kJoystickConversionFactor, right);
+	Vector left, right;
+	Vector2DMake(joystick.joy2_x1 * kJoystickConversionFactor, joystick.joy2_y1 * kJoystickConversionFactor * -1.0, left);
+	Vector2DMake(joystick.joy2_x2 * kJoystickConversionFactor, joystick.joy2_y2 * kJoystickConversionFactor * -1.0, right);
 
 	memcpy(c.leftJoystick, left, sizeof(Vector));
 	memcpy(c.rightJoystick, right, sizeof(Vector));
 
-
-	//	FIXME: D-Pad?????????????????????????????????????????
-
+	//	get D-Pad
+	_UpdateControllerDPad(c, joystick.joy2_TopHat);
+	
+	//	get buttons
 	_UpdateControllerButtons(c, joystick.joy2_Buttons);
 }
+
+
+
--- Common/Controller.h
+++ Common/Controller.h
@@ -1,8 +1,7 @@
 
 
-
-#ifndef _CustomJoystick_
-#define _CustomJoystick_
+#ifndef _Controller_
+#define _Controller_
 
 
 #ifndef _Vector_
@@ -17,37 +16,60 @@ typedef enum {
 	ControllerButton2                   = 1,
 	ControllerButton3                   = 2,
 	ControllerButton4                   = 3,
+	
 	ControllerButtonR1                  = 5,
 	ControllerButtonR2                  = 7,
+	
 	ControllerButtonL1                  = 4,
 	ControllerButtonL2                  = 6,
+	
 	ControllerButtonLeftMiddle          = 8,
 	ControllerButtonRightMiddle         = 9,
+	
 	ControllerButtonRightJoystickPress  = 11,
 	ControllerButtonLeftJoystickPress   = 10
 } ControllerButton;
 
-#define kControllerButtonCount ControllerButtonL2 + 1
 
 
 
 typedef struct {
-	Vector leftJoystick;	//	components are percentages
-	Vector rightJoystick;
-	short buttons;			// Bit map for 12-buttons
+	Vector leftJoystick;	//	components are from 0 to 1
+	Vector rightJoystick;	//	components are from 0 to 1
+	Vector dPad;			//	components are either 0 or 1
+	short buttons;			//	Bit map for 12-buttons
 	short buttonsPrevious;
-	short toggleButtons;	//
+	short toggleButtons;
 } Controller;
 
 
 
 
+/*
+ Axis for all directional values (Vectors)
+ 
+        *
+        *
+ (-,+)	*	(+,+)
+        *
+ *****************
+        *
+ (-,-)	*	(+,-)
+        *
+        *
+ 
+ */
+
+
+
+
+bool ControllerButtonIsPressed(Controller& c, ControllerButton b);		//	returns true if it is currently pressed
+bool ControllerButtonChanged(Controller& c, ControllerButton b);		//	returns yes if the button changed since the last update
+bool ControllerButtonIsToggledOn(Controller& c, ControllerButton b);	//	the controller tracks toggled buttons.  pressing a button toggles it on.  pressing it again toggles it off.
 
 
 void UpdatePrimaryController(Controller& c);
 void UpdateSecondaryController(Controller& c);
-
-
 
 
 
