c498609ee8239a06e28f8a6e11cdf0e7a3e93f37
Justin Buchanan <justbuchanan@gmail.com>
2010-10-25 23:30:40 -0400

    tweaked defines in map.c

--- Map.c
+++ Map.c
@@ -222,6 +222,14 @@ void _MapResetDijkstraAlgorithmCache()
 }
 
 
+void _MapClearCachedPath()
+{
+	for ( NodeID n = 0; n < kNodeCount; n++ )
+	{
+		globalMap.cachedPath[n] = NodeIDZero;
+	}
+}
+
 
 //	this function is here b/c robotc doesn't allow recursive functions.	 this is just an easy work-around
 void _MapTravelNode(NodeID nodeID);
@@ -266,31 +274,7 @@ void _MapTravelNode(NodeID nodeID)
 }
 
 
-void _MapPrintPath()
-{
-	//printf("\n\npath:\n");
 
-	for ( NodeID n = 0; n < kNodeCount; n++ )
-	{
-		NodeID id = globalMap.cachedPath[n];
-		if ( id == NodeIDZero ) break;	//	stop if we hit the end
-
-		//printf("%s", globalMap.nodes[id].name);
-		//printf("\n");
-	}
-
-	//printf("\n");
-}
-
-
-
-void _MapClearCachedPath()
-{
-	for ( NodeID n = 0; n < kNodeCount; n++ )
-	{
-		globalMap.cachedPath[n] = NodeIDZero;
-	}
-}
 
 
 void _MapFindShortestPath(NodeID from, NodeID to)
@@ -329,6 +313,24 @@ void _MapFindShortestPath(NodeID from, NodeID to)
 	globalMap.cached = true;	//	we just cached the results
 }
 
+
+
+
+void _MapPrintPath()
+{
+	//printf("\n\npath:\n");
+
+	for ( NodeID n = 0; n < kNodeCount; n++ )
+	{
+		NodeID id = globalMap.cachedPath[n];
+		if ( id == NodeIDZero ) break;	//	stop if we hit the end
+
+		//printf("%s", globalMap.nodes[id].name);
+		//printf("\n");
+	}
+
+	//printf("\n");
+}
 
 
 
@@ -489,8 +491,7 @@ void MapSetNodeNameForID(NodeID nodeID, string name)
 //	note: width is the dimension that runs along the x-axis, and length runs along the y-axis
 
 
-#define kFieldWidth 144
-#define kFieldLength 144
+#define kFieldSize 144
 
 
 #define kDispenserPerpendicularDistance 10	//	distance from a dispenser to its corresponding node
@@ -528,13 +529,13 @@ void MapInit()		//	sets values specific to our field.
 	Vector2DMake(kStartSquareWidth / 2, kStartSquareWidth / 2, location);
 	MapSetNodeInfoForID(NodeIDRedStartSquareLeft, "R left start sqr", location);
 
-	Vector2DMake(kFieldWidth - (kStartSquareWidth / 2), kStartSquareWidth / 2, location);
+	Vector2DMake(kFieldSize - (kStartSquareWidth / 2), kStartSquareWidth / 2, location);
 	MapSetNodeInfoForID(NodeIDRedStartSquareRight, "R right start sqr", location);
 
-	Vector2DMake(kStartSquareWidth / 2, kFieldLength - (kStartSquareWidth / 2), location);
+	Vector2DMake(kStartSquareWidth / 2, kFieldSize - (kStartSquareWidth / 2), location);
 	MapSetNodeInfoForID(NodeIDBlueStartSquareLeft, "B left start sqr", location);
 
-	Vector2DMake(kFieldWidth - (kStartSquareWidth / 2), kFieldLength - (kStartSquareWidth / 2), location);
+	Vector2DMake(kFieldSize - (kStartSquareWidth / 2), kFieldSize - (kStartSquareWidth / 2), location);
 	MapSetNodeInfoForID(NodeIDBlueStartSquareRight, "B right start sqr", location);
 
 
@@ -542,61 +543,61 @@ void MapInit()		//	sets values specific to our field.
 	Vector2DMake(kDispenserPerpendicularDistance, kSideDispenserDistanceFromFieldEnd, location);
 	MapSetNodeInfoForID(NodeIDRedDispenserLeft, "R left dispenser", location);
 
-	Vector2DMake(kFieldWidth / 2, kFieldLength - kDispenserPerpendicularDistance, location);
+	Vector2DMake(kFieldSize / 2, kFieldSize - kDispenserPerpendicularDistance, location);
 	MapSetNodeInfoForID(NodeIDRedDispenserCenter, "R cntr dispenser", location);
 
-	Vector2DMake(kFieldWidth - kDispenserPerpendicularDistance, kSideDispenserDistanceFromFieldEnd, location);
+	Vector2DMake(kFieldSize - kDispenserPerpendicularDistance, kSideDispenserDistanceFromFieldEnd, location);
 	MapSetNodeInfoForID(NodeIDRedDispenserRight, "R right dispenser", location);
 
-	Vector2DMake(kDispenserPerpendicularDistance, kFieldLength - kSideDispenserDistanceFromFieldEnd, location);
+	Vector2DMake(kDispenserPerpendicularDistance, kFieldSize - kSideDispenserDistanceFromFieldEnd, location);
 	MapSetNodeInfoForID(NodeIDBlueDispenserLeft, "B left dispenser", location);
 
-	Vector2DMake(kFieldWidth / 2, kDispenserPerpendicularDistance, location);
+	Vector2DMake(kFieldSize / 2, kDispenserPerpendicularDistance, location);
 	MapSetNodeInfoForID(NodeIDBlueDispenserCenter, "B cntr dispenser", location);
 
-	Vector2DMake(kFieldWidth - kDispenserPerpendicularDistance, kFieldLength - kSideDispenserDistanceFromFieldEnd, location);
+	Vector2DMake(kFieldSize - kDispenserPerpendicularDistance, kFieldSize - kSideDispenserDistanceFromFieldEnd, location);
 	MapSetNodeInfoForID(NodeIDBlueDispenserRight, "B right dispenser", location);
 
 
 
 	/**********		Bridges		**********/
-	Vector2DMake(kFieldWidth - kBridgeDistanceFromSide, (kFieldLength / 2) + (kBridgeLength / 2), location);
+	Vector2DMake(kFieldSize - kBridgeDistanceFromSide, (kFieldSize / 2) + (kBridgeLength / 2), location);
 	MapSetNodeInfoForID(NodeIDRedBridgeTop, "R bridge top", location);
 
-	VectorMake(kFieldWidth - kBridgeDistanceFromSide, kFieldLength / 2, kBridgeHeight, location);
+	VectorMake(kFieldSize - kBridgeDistanceFromSide, kFieldSize / 2, kBridgeHeight, location);
 	MapSetNodeInfoForID(NodeIDRedBridgeCenter, "R bridge cntr", location);
 
-	Vector2DMake(kFieldWidth - kBridgeDistanceFromSide, (kFieldLength / 2) - (kBridgeLength / 2), location);
+	Vector2DMake(kFieldSize - kBridgeDistanceFromSide, (kFieldSize / 2) - (kBridgeLength / 2), location);
 	MapSetNodeInfoForID(NodeIDRedBridgeBottom, "R bridge btm", location);
 
-	Vector2DMake(kBridgeDistanceFromSide, (kFieldLength / 2) + (kBridgeLength / 2), location);
+	Vector2DMake(kBridgeDistanceFromSide, (kFieldSize / 2) + (kBridgeLength / 2), location);
 	MapSetNodeInfoForID(NodeIDBlueBridgeTop, "B bridge top", location);
 
 
-	VectorMake(kBridgeDistanceFromSide, kFieldLength / 2, kBridgeHeight, location);
+	VectorMake(kBridgeDistanceFromSide, kFieldSize / 2, kBridgeHeight, location);
 	MapSetNodeInfoForID(NodeIDBlueBridgeCenter, "B bridge cntr", location);
 
-	Vector2DMake(kBridgeDistanceFromSide, (kFieldLength / 2) - (kBridgeLength / 2), location);
+	Vector2DMake(kBridgeDistanceFromSide, (kFieldSize / 2) - (kBridgeLength / 2), location);
 	MapSetNodeInfoForID(NodeIDBlueBridgeBottom, "B bridge btm", location);
 
 
 
 	/**********		Pit Goals		**********/
-	Vector2DMake(kFieldWidth - (kPitWidth / 2), (kFieldLength / 2) - (kPitLength / 2), location);
+	Vector2DMake(kFieldSize - (kPitWidth / 2), (kFieldSize / 2) - (kPitLength / 2), location);
 	MapSetNodeInfoForID(NodeIDRedPitCenter, "R pit goal cntr", location);
 
-	Vector2DMake(kPitWidth / 2, (kFieldLength / 2) + (kPitLength / 2), location);
+	Vector2DMake(kPitWidth / 2, (kFieldSize / 2) + (kPitLength / 2), location);
 	MapSetNodeInfoForID(NodeIDBluePitCenter, "B pit goal cntr", location);
 
 
 	/**********		Mountain		**********/
-	Vector2DMake(kFieldWidth / 2, (kFieldLength / 2) + (kMountainLength / 2), location);
+	Vector2DMake(kFieldSize / 2, (kFieldSize / 2) + (kMountainLength / 2), location);
 	MapSetNodeInfoForID(NodeIDMountainCenterTopEdge, "mntn cntr top edge", location);
 
-	VectorMake(kFieldWidth / 2, kFieldLength / 2, kMountainHeight, location);
+	VectorMake(kFieldSize / 2, kFieldSize / 2, kMountainHeight, location);
 	MapSetNodeInfoForID(NodeIDMountainCenterPeak, "mntn cntr peak", location);
 
-	Vector2DMake(kFieldWidth / 2, (kFieldLength / 2) - (kMountainLength / 2), location);
+	Vector2DMake(kFieldSize / 2, (kFieldSize / 2) - (kMountainLength / 2), location);
 	MapSetNodeInfoForID(NodeIDMountainCenterBottomEdge, "mntn cntr btm edge", location);
 
 
