b42f3dd74d3443b48bdcb683118be30ebe6f3aa9
Justin Buchanan <justbuchanan@gmail.com>
2010-11-07 19:07:15 -0500

    fixed errors

--- Autonomous/RobotControl.c
+++ Autonomous/RobotControl.c
@@ -44,27 +44,27 @@ bool RobotFindWhiteLine(LineFollowingContext& ctxtOut)	//	returns true if it fin
 
 void RobotRotateToOrientation(float orientation)
 {
-	float currentOrientation = _currentRobotPosition.orientation;
+	float currentOrientation = currentRobotPosition.orientation;
 	float angle = orientation - currentOrientation;
-	
+
 	//	reduce angle
 	if ( abs(angle) > 2 * PI )
 	{
 		if ( angle < 0 ) angle += 2 * PI;
 		else angle -= 2 * PI;
 	}
-	
+
 	float wheelDistance = angle * (kRobotWidth / 2);
-	
+
 	//	start rotating
 	motor[Left] = ( wheelDistance > 0 ) ? kRobotRotateSpeed : -kRobotRotateSpeed;
 	motor[Right] = -motor[Left];
-	
+
 	int encoderPoints = TetrixConvertDistanceToEncoder(wheelDistance);
 	nMotorEncoder[Left] = 0;
-	
+
 	while ( abs(nMotorEncoder[Left]) < abs(encoderPoints) ) {}	//	wait until we're done
-	
+
 	//	stop
 	motor[Left] = 0;
 	motor[Right] = 0;
@@ -75,13 +75,13 @@ void RobotRotateToOrientation(float orientation)
 bool RobotMoveDistance(float distance)
 {
 	int encoderPoints = TetrixConvertDistanceToEncoder(distance);
-	
+
 	motor[Left] = ( distance > 0 ) ? kRobotMoveSpeed : -kRobotMoveSpeed;
 	motor[Right] = motor[Left];
-	
+
 	while ( abs(nMotorEncoder[Left]) < abs(encoderPoints) ) {}	//	wait until we're done
-	
-	
+
+
 	return true;	//	FIXME: this value should mean asometihgn	///////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
@@ -100,19 +100,3 @@ void RobotBalance()
 {
   //  FIXME: implement
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
